d93a2917c32467a31e6babccaa75b24983d7cc87
Harshvardhan Singh Sisodiya <harshvardhan.intern@aivhub.com>
2024-05-04 19:26:42 +0530

    Updated code for the palindrome Number

---
+++ palindromeNumber.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        if x == 0:
+            return True
+        if x % 10 == 0:
+            return False
+        
+        originalX = x
+        numReversed = 0
+        while x > 0:
+            lastDigit = x % 10 
+            numReversed = numReversed * 10 + lastDigit
+            x = x // 10 
+            
+        return numReversed == originalX
+sol = Solution()
+print(sol.isPalindrome(121))
+print(sol.isPalindrome(-121))
+print(sol.isPalindrome(10))
