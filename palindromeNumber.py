9db6d1453f4f7b45e7e95bd188187bf98572f2cc
Harshvardhan Singh Sisodiya <harshvardhan.intern@aivhub.com>
2024-05-04 19:48:52 +0530

    Updated code for the palindrome Number

--- palindromeNumber.py
+++ palindromeNumber.py
@@ -1,9 +1,13 @@
+# A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward
 class Solution(object):
     def isPalindrome(self, x):
+        # Check if x is negative, negative numbers can't be palindromes
         if x < 0:
             return False
+        # Check if x is 0, 0 is considered a palindrome
         if x == 0:
             return True
+        # If the last digit of x is 0 and x is not 0 itself, it can't be a palindrome
         if x % 10 == 0:
             return False
         
@@ -16,6 +20,6 @@ class Solution(object):
             
         return numReversed == originalX
 sol = Solution()
-print(sol.isPalindrome(121))
-print(sol.isPalindrome(-121))
-print(sol.isPalindrome(10))
+print(sol.isPalindrome(121)) # true
+print(sol.isPalindrome(-121)) # false
+print(sol.isPalindrome(10)) # false
